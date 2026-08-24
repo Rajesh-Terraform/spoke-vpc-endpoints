20ab7a5d104ecf1f74f97374a2bfe9eb621edb4e
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-24 14:48:08 +0530

    spoke vpc endpoints

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,65 @@
+variable "aws_region" {
+  description = "AWS region where the spoke VPC will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "spoke-vpc"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the spoke VPC"
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "private_subnet_a_cidr" {
+  description = "CIDR block for private subnet A"
+  type        = string
+  default     = "10.20.1.0/24"
+}
+
+variable "private_subnet_b_cidr" {
+  description = "CIDR block for private subnet B"
+  type        = string
+  default     = "10.20.2.0/24"
+}
+
+variable "availability_zone_a" {
+  description = "Availability Zone A"
+  type        = string
+  default     = "ap-south-1a"
+}
+
+variable "availability_zone_b" {
+  description = "Availability Zone B"
+  type        = string
+  default     = "ap-south-1b"
+}
+
 variable "github_org" {
   description = "GitHub organization or username"
   type        = string
+  default     = ""
 }
 
 variable "github_repository" {
   description = "GitHub repository name"
   type        = string
+  default     = ""
 }
 
 variable "github_branch" {
-  description = "Branch allowed to deploy"
+  description = "GitHub branch allowed to deploy"
   type        = string
   default     = "main"
 }
